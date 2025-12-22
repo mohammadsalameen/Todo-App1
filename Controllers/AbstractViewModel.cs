0f010ace4667156de1a405fac34ac0df5f8539f4
mohammadsalameen <mohammadsalameen.02@gmail.com>
2025-12-22 10:46:58 +0200

    first commit

---
+++ Controllers/AbstractViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Todo_App.Controllers
+{
+    public class AbstractViewModel
+    {
+        public bool success { get; set; }
+        public int Id { get; set; }
+        public List<string> lstErrors { get; set; } = new List<string>();
+        public List<string> lstWarning { get; set; } = new List<string>();
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,53 @@
+﻿using Todo_App.DataAccess;
+namespace Todo_App
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add controllers
+            services.AddControllers();
+
+            //services.AddApplication();
+
+            // Add DB access
+            services.AddDataAccess(Configuration);
+
+            // Add OpenAPI/Swagger
+            services.AddOpenApi();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                //app.UseOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
