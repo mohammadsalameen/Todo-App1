a5a4a7560b4f6a5074152a298e81353c05efb6da
mohammadsalameen <mohammadsalameen.02@gmail.com>
2026-01-06 12:50:38 +0200

    add-task and add-comment endpoints

---
+++ Todo_App/Controllers/CommentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Todo_App.Application.Comments.Commands;
+
+namespace Todo_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ApiController
+    {
+        [HttpPost()]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Create([FromBody] CreateCommentCommand req) =>
+        ResponseToFE(await Mediator.Send(req));
+
+        [HttpGet("my-comments")]
+        public async Task<IActionResult> GetAll() =>
+        Ok(await Mediator.Send(new GetCommentsQuery()));
+    }
+}
